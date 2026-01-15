3272f257d732f8e729cd004cadccd48bd78897c1
vCentDev <vicentDev@yahoo.com>
2026-01-15 11:06:55 +0100

    feat(traffic light): implement TrafficLight component and update main entry point

---
+++ src/01-useState/TrafficLight.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+const colors = {
+    red: 'bg-red-500',
+    yellow: 'bg-yellow-500',
+    green: 'bg-green-500'
+}
+
+type TrafficLightColor = keyof typeof colors;
+
+export const TrafficLight = () => {
+    const [light, setLight] = useState<TrafficLightColor>('red')
+
+    const handleColorChange = (color: TrafficLightColor) => {
+        setLight(color);
+    }
+
+    return (
+        <div className="min-h-screen bg-linear-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
+            <div className="flex flex-col items-center space-y-8">
+                <div className={`w-32 h-32 ${light === 'red' ? colors.red : 'bg-gray-500'} rounded-full`}></div>
+                <div className={`w-32 h-32 ${light === 'yellow' ? colors.yellow : 'bg-gray-500'} rounded-full`}></div>
+                <div className={`w-32 h-32 ${light === 'green' ? colors.green : 'bg-gray-500'} rounded-full`}></div>
+
+                {/* Botón para cambiar el estado de la luz */}
+                <div className="flex gap-2">
+                    <button
+                        onClick={() => handleColorChange('red')}
+                        className="bg-red-500 text-white px-4 py-2 rounded-md cursor-pointer">
+                        Rojo
+                    </button>
+                    <button
+                        onClick={() => handleColorChange('yellow')}
+                        className="bg-yellow-500 text-white px-4 py-2 rounded-md cursor-pointer">
+                        Amarillo
+                    </button>
+                    <button
+                        onClick={() => handleColorChange('green')}
+                        className="bg-green-500 text-white px-4 py-2 rounded-md cursor-pointer">
+                        Verde
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import { HooksApp } from './HooksApp';
+// import { HooksApp } from './HooksApp';
 
 import './index.css'
+import { TrafficLight } from './01-useState/TrafficLight'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HooksApp />
+    {/* <HooksApp /> */}
+    <TrafficLight />
   </StrictMode>,
 )
