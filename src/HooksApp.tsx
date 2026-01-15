8417908f7b3f5248e60d4d63a2783d08a7ff5dc1
vCentDev <vicentDev@yahoo.com>
2026-01-15 10:27:34 +0100

    feat(tailwind & HooksApp): initial Vite configuration, create HooksApp component, and set up Tailwind CSS styles

---
+++ src/HooksApp.tsx
@@ -0,0 +1,8 @@
+
+export const HooksApp = () => {
+    return (
+        <div className="bg-gradient">
+            <h1 className="text-3xl font-bold underline">Hooks App!</h1>
+        </div>
+    )
+}
