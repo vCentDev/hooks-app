5c3f01524613c2b2ee99e75d6adb57457a0e6eb0
vCentDev <vicentDev@yahoo.com>
2026-01-20 17:03:26 +0100

    feat(scramble words): add ScrambleWords component for a word guessing game and update main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,8 @@ import { createRoot } from 'react-dom/client'
 // import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect'
 // import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook'
 // import { PokemonPage } from './03-examples/PokemonPage'
-import { TasksApp } from './05-useReducer/TaskApp'
+// import { TasksApp } from './05-useReducer/TaskApp'
+import { ScrambleWords } from './05-useReducer/ScrambleWords'
 
 import './index.css'
 
@@ -17,6 +18,7 @@ createRoot(document.getElementById('root')!).render(
     {/* <TrafficLightWithEffect /> */}
     {/* <TrafficLightWithHook /> */}
     {/* <PokemonPage /> */}
-    <TasksApp />
+    {/* <TasksApp /> */}
+    <ScrambleWords />
   </StrictMode>,
 )
