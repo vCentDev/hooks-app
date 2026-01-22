c037e95748b10bac01529f49e12995fd91321547
vCentDev <vicentDev@yahoo.com>
2026-01-22 09:28:48 +0100

    feat(dependencies): add sonner library for toast notifications

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Toaster } from 'sonner'
 
 // import { HooksApp } from './HooksApp';
 // import { TrafficLight } from './01-useState/TrafficLight'
@@ -16,6 +17,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Toaster />
     {/* <HooksApp /> */}
     {/* <TrafficLight /> */}
     {/* <TrafficLightWithEffect /> */}
